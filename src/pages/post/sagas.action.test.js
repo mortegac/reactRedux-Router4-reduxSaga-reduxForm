1789dc5b13f2266d26db6ce31e5f4e2b0b0c588f
Manuel Ortega Carcamo <mortegac@gmail.com>
2017-08-06 12:00:34 -0400

    Se me habia olvidado subr el test

---
+++ src/pages/post/sagas.action.test.js
@@ -0,0 +1,51 @@
+/******************************************
+*  TEST PARA sagas.action.js
+*******************************************/
+import assert from 'assert'
+import axios from 'axios'
+import { call, put } from 'redux-saga/effects'
+import posts from './sagas.action'
+
+describe('Fetch actions', () => {
+
+     describe('#fetch', () => {
+        it('catches errors', () => {
+            const gen = posts.fetch()
+
+            //Fetch todo start
+            assert.deepEqual(
+                gen.next().value,
+                put({ type: 'FETCH_POST_START' }),
+                'No se disparó el evento'
+            )
+
+            //Call to API - FETCH_POST_SUCCESS
+            assert.deepEqual(
+                gen.next().value,
+                call(
+                axios.get,
+                'https://jsonplaceholder.typicode.com/posts'
+                ),
+                'No retorna los Post'
+            )
+
+            //Failed
+            // assert.deepEqual(
+            //     gen.throw('Provoco Error').value,
+            //     put({
+            //     type: 'FETCH_POST_FAILED',
+            //     error: 'Fallo',
+            //     }), 'No dispara el evento que manejo el error'
+            // )
+
+
+        })
+    })
+
+
+            
+          
+
+
+})
+
