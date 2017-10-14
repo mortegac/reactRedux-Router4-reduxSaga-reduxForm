e69c5b277c55af5e1811f60234514559f5bf4209
levelasquez <velasquezleonardo1@gmail.com>
2017-10-14 15:39:47 -0300

    Fix sagas integration

--- src/index.js
+++ src/index.js
@@ -1,60 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { createStore, applyMiddleware } from 'redux'
-// import { reducer as formReducer } from 'redux-form'
-import createSagaMiddleware from 'redux-saga'
-
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import createSagaMiddleware from 'redux-saga'
 import createHistory from 'history/createBrowserHistory'
-// import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
-import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import {
+  createStore,
+  applyMiddleware,
+} from 'redux'
 
 import rootSagas from './pages/post/sagas'
 import rootReducers from './app/reducer'
+import App from './app/App'
 
-
-import App from './app/App';
-import registerServiceWorker from './registerServiceWorker';
-
-
-const history    = createHistory()              // Create a history of your choosing (we're using a browser history in this case)
-const routerHistoryMiddleware = routerMiddleware(history)
+const history = createHistory()
 const sagaMiddleware = createSagaMiddleware()
-
-// let midlewares = [ routerHistoryMiddleware, sagaMiddleware ]
-// let midlewares = [ routerHistoryMiddleware, sagaMiddleware ]
-const midlewares = (routerHistoryMiddleware, sagaMiddleware) => 
-                    value => routerHistoryMiddleware(
-                                                      sagaMiddleware(value)
-                                                    )
-
-let store = createStore(
-    rootReducers,
-    applyMiddleware(
-                    midlewares(routerHistoryMiddleware, sagaMiddleware)
-                  )
-
+const store = createStore(
+  rootReducers,
+  applyMiddleware(sagaMiddleware)
 )
-// console.log('rootSagas', rootSagas)
-// console.log('store', store)
 
 sagaMiddleware.run(rootSagas)
-  .done.catch((error) => console.log(error));
-
-//store.runSaga = sagaMiddleware.run(rootSagas)
-// sagaMiddleware.run(rootSagas)
-
 
 ReactDOM.render(
   <Provider store={store}>
-    { /* ConnectedRouter will use the store from Provider automatically */ }
-    <ConnectedRouter history={history} sagaMiddleware={sagaMiddleware}>
-        <App />
+    <ConnectedRouter history={history}>
+      <App />
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 )
-
-
-registerServiceWorker();
